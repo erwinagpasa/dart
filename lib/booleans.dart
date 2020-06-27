c84b61d755c843220879df9e46b0b5d4fd6339a5
erwinagpasa <istran.net@gmail.com>
2020-06-27 22:30:30 +0300

    boolean

---
+++ lib/booleans.dart
@@ -0,0 +1,17 @@
+void main(List<String> arguments) {
+  /*
+  bool is a data type
+  isOn is a variable - something that will change
+  */
+  bool isOn; //this is a null object
+  //you can also declare a initial value ex: bool isOn = true or var isOn = true
+  print('isOn = ${isOn}');
+
+  isOn = true; 
+  print('isOn = ${isOn}');
+
+  isOn = false; 
+  print('isOn = ${isOn}');
+
+
+}
