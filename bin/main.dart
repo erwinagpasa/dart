7a523f2287ee7a258684563ffadccbebe1097fb4
erwinagpasa <istran.net@gmail.com>
2020-07-04 11:13:00 +0300

    list

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,1 @@
-//enum should declare outside of the main() function
-enum colors { red, green, blue }
-void main(List<String> arguments) {
-  print(colors.values); // output [colors.red, colors.green, colors.blue]
-  print(colors.green); // output colors.green
-}
+void main(List<String> arguments) {}
