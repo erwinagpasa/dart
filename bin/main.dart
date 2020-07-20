d35199155975f09a640cbcd1117914a54fc26a0d
erwinagpasa <istran.net@gmail.com>
2020-07-20 23:13:26 +0300

    Functions - Optional Named Parameters

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,17 @@
 void main(List<String> arguments) {
-  add(2, 3); //Output 5
+  add(2, 3); //output: 2 3 null
+  sample(5, num2: 6); //output: 5 6 null
+  sample(5, num2: 6, num3: 7); //output: 5 6 7
 }
 
-void add(int num1, int num2) {
-  print(num1 + num2);
+void add(int num1, [int num2, int num3]) {
+  print(num1);
+  print(num2);
+  print(num3); //null
+}
+
+void sample(int num1, {int num2, int num3}) {
+  print(num1);
+  print(num2);
+  print(num3); //null
 }
