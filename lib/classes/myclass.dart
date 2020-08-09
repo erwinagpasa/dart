fe72358b00d186d6a2ed8a2ddc57d43abf507fd5
erwinagpasa <istran.net@gmail.com>
2020-08-09 09:19:18 +0300

    introduction to class

---
+++ lib/classes/myclass.dart
@@ -0,0 +1,10 @@
+class MyClass {
+  /*
+  void sayHello(String name) {
+    print('Hello ${name}');
+  }
+  */
+
+  //This is a short hand function
+  void sayHello(String name) => print('Hello ${name}');
+}
